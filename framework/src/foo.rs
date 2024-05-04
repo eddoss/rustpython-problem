f80ced5f9467b08c5c08faea77c2f4a52ae6e5ad
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2024-05-04 19:49:53 +0200

    (runtime-error) Add 'Constructor'

--- framework/src/foo.rs
+++ framework/src/foo.rs
@@ -2,7 +2,7 @@ use rustpython::vm::{pymodule, VirtualMachine, PyRef, builtins::PyModule};
 
 #[pymodule]
 pub mod _module {
-    use rustpython_vm::common::lock::PyMutex;
+    use rustpython_vm::common::lock::PyRwLock;
     use rustpython::vm::{
         builtins::PyStr,
         PyPayload,
@@ -10,30 +10,62 @@ pub mod _module {
         pyclass,
         VirtualMachine,
     };
-    use rustpython_vm::builtins::PyStrRef;
-    use rustpython_vm::function::PySetterValue;
+    use rustpython_vm::builtins::{PyStrRef};
+    use rustpython_vm::function::{PySetterValue};
+    use rustpython_vm::{Py, PyResult};
+    use rustpython_vm::types::{Constructor, Unconstructible, Representable};
 
     #[pyattr]
     #[pyclass(name = "Foo")]
-    #[derive(Debug, PyPayload)]
-    struct Foo {
-        name: PyMutex<String>,
+    #[derive(Debug, PyPayload, Default)]
+    pub struct Foo {
+        name: PyRwLock<String>,
     }
 
-    #[pyclass]
+
+    // CODE BELLOW DOES NOT COMPILE
+    // note: conflicting implementation in crate `rustpython_vm`:
+    //       - impl<T> Constructor for T
+    //       where T: Unconstructible;
+
+    // impl Constructor for Foo {
+    //     type Args = FuncArgs;
+    //
+    //     fn py_new(cls: PyTypeRef, _args: Self::Args, vm: &VirtualMachine) -> PyResult {
+    //         Err(vm.new_type_error(format!("cannot create {} instances", cls.slot_name())))
+    //     }
+    // }
+
+    impl Unconstructible for Foo {}
+
+    impl Representable for Foo {
+        #[inline]
+        fn repr(_zelf: &Py<Self>, vm: &VirtualMachine) -> PyResult<PyStrRef> {
+            const REPR: &str = "<Foo object at .. >";
+            Ok(vm.ctx.intern_str(REPR).to_owned())
+        }
+
+        #[cold]
+        fn repr_str(_zelf: &Py<Self>, _vm: &VirtualMachine) -> PyResult<String> {
+            unreachable!("use repr instead")
+        }
+    }
+
+
+    #[pyclass(with(Constructor))]
     impl Foo {
         #[pygetset]
         fn name(&self) -> PyStr {
-            PyStr::from(self.name.lock().as_str())
+            PyStr::from(self.name.read().as_str())
         }
 
         #[pygetset(setter)]
         pub fn set_name(&self, value: PySetterValue<PyStrRef>, _: &VirtualMachine) {
-            let mut n = self.name.lock();
+            let mut n = self.name.write();
             *n = value.unwrap().as_str().to_string();
         }
-
     }
+
 }
 
 pub fn module(vm: &VirtualMachine) -> PyRef<PyModule> {
