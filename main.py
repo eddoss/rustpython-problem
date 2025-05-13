be98aa6feb8e10c79dbc0e74fcf531625e78aa2a
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2025-05-13 22:43:22 +0200

    Problem fixed and closed

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import problem
-
-foo = problem.Foo(
-    items=["one", "two"]
-)
-foo.items.append("free")    # this does not work
-print(foo.items)            # expect: ['one', 'two', 'free']
-                            # actual: ['one', 'two']
+foo = problem.Foo()
+foo.items.append("free")
+print(foo.items)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::borrow::Cow::Borrowed;
 use ahash::HashMapExt;
 use rustpython::InterpreterConfig;
-use rustpython::vm::{
-    pymodule,
-    stdlib::StdlibMap, 
-    convert::ToPyResult
-};
+use rustpython::vm::{pymodule, stdlib::StdlibMap};
+use std::borrow::Cow::Borrowed;
 
 fn main() {
     // Create stdlib and emplace a framework to this.
@@ -32,39 +28,32 @@ fn main() {
     // Execute ./main.py
     interpreter.enter(|vm| match vm.import("main", 0) {
         Ok(_) => {}
-        Err(e) => {
-            let text = e.to_pyresult(vm).unwrap().str(vm).unwrap().to_string();
-            println!("{text}")
-        }
+        Err(e) => vm.print_exception(e),
     });
 }
 
 #[pymodule]
 pub mod _module {
     use rustpython::vm::{
-        FromArgs,
-        VirtualMachine,
-        PyPayload,
-        PyRef,
-        PyResult,
-        types::{Constructor, DefaultConstructor, Initializer},
-        builtins::{PyStr, PyList, PyListRef},
-        pyclass,
+        FromArgs, PyPayload, PyRef, PyResult, VirtualMachine,
+        builtins::{PyList, PyListRef, PyStr},
+        class::StaticType,
         common::lock::PyRwLock,
+        pyclass,
+        types::{Constructor, DefaultConstructor, Initializer},
+        AsObject,
     };
 
     #[pyattr]
     #[pyclass(name = "Foo")]
     #[derive(Debug, PyPayload)]
     pub struct Foo {
-        pub items: PyRwLock<PyList>,
+        pub items: PyRwLock<PyListRef>,
     }
 
     impl Default for Foo {
         fn default() -> Self {
-            Foo {
-                items: PyRwLock::new(PyList::default()),
-            }
+            Foo { items: PyRwLock::new(PyListRef::new_ref(PyList::default(), PyList::create_static_type(), None)) }
         }
     }
 
@@ -80,16 +69,13 @@ pub mod _module {
         type Args = FooInitArgs;
 
         fn init(zelf: PyRef<Self>, args: Self::Args, vm: &VirtualMachine) -> PyResult<()> {
-            match args.items {
-                // Allow only strings
-                None => *zelf.items.write() = PyList::default(),
-                Some(strings) => {
-                    let refs: Vec<_> = strings
-                        .iter()
-                        .map(|s| PyStr::from(s.as_str()).into_pyobject(vm))
-                        .collect();
-                    *zelf.items.write() = PyList::from(refs);
-                }
+            *zelf.items.write() = vm.ctx.new_list(vec![]);
+            if let Some(entries) = args.items {
+                let references: Vec<_> = entries
+                    .into_iter()
+                    .map(|s| PyStr::from(s).into_pyobject(vm))
+                    .collect();
+                *zelf.items.write() = vm.ctx.new_list(references);
             }
             Ok(())
         }
@@ -98,23 +84,16 @@ pub mod _module {
     #[pyclass(with(Constructor, Initializer))]
     impl Foo {
         #[pygetset]
-        pub fn items(&self, vm: &VirtualMachine) -> PyListRef {
-            vm.ctx
-                .new_list(self.items.write().borrow_vec().to_vec())
-                .into()
+        fn items(&self, _: &VirtualMachine) -> PyListRef {
+            self.items.read().clone()
         }
 
         #[pygetset(setter)]
-        pub fn set_items(&self, value: PyListRef, vm: &VirtualMachine) -> PyResult<()> {
-            value.borrow_vec().iter().for_each(|v| {
-                match v.downcast_ref::<PyStr>() {
-                    Some(_) => {}
-                    None => {
-                        panic!("Expect strings, but given {:?}", v);
-                    }
-                }
-            });
-            *self.items.write() = PyList::from(value.borrow_vec().to_vec());
+        fn set_items(&self, value: PyListRef, vm: &VirtualMachine) -> PyResult<()> {
+            if value.borrow_vec().iter().all(|v|v.fast_isinstance(&*PyStr::create_static_type())) {
+                return Err(vm.new_type_error("expected list[str]".to_string()));
+            }
+            *self.items.write() = value;
             Ok(())
         }
     }
